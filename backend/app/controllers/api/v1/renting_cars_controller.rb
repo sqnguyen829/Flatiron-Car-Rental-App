d24b123409db8f2a3a4bae8ad98101280b52c1e5
github username <github email address>
2019-12-04 14:35:35 -0600

    renting car tab works, working on patch issue of changing availible boolean

--- backend/app/controllers/api/v1/renting_cars_controller.rb
+++ backend/app/controllers/api/v1/renting_cars_controller.rb
@@ -1,4 +1,19 @@
 class Api::V1::RentingCarsController < ApplicationController
+
+    def index
+        rentingCars = RentingCar.all
+        render json: rentingCars, except: [:created_at, :updated_at]
+    end
+
+    def show
+        rentingCar = RentingCar.find_by(id: params[:id])
+        if car 
+            render json: rentingCar, except: [:created_at, :updated_at]
+        else
+            render json: {message: 'car not found'}
+        end
+    end
+
     def create
         rentingCar = RentingCar.create(rentingCar_params)
         render json: rentingCar, except: [:created_at, :updated_at]
@@ -10,7 +25,7 @@ class Api::V1::RentingCarsController < ApplicationController
     end
 
     private
-    def car_params
+    def rentingCar_params
         params.require(:rentingCar).permit(:user_id,:car_id,:start_date,:end_date,:cost)
     end
 end
--- frontend/src/components/ShowCarDetails.js
+++ frontend/src/components/ShowCarDetails.js
@@ -4,11 +4,11 @@ export default function ShowCarDetails(props){
     return(
         <div class="ui items">
             <div class="item">
-            <div class="ui medium image">
-                <img src={props.car.image}></img>
-            </div>
-            <div class="content">
-                <div class="header"> {props.car.make} {props.car.model}</div>
+                <div class="ui medium image">
+                    <img src={props.car.image}></img>
+                </div>
+                <div class="content">
+                    <div class="header"> {props.car.make} {props.car.model}</div>
                     <div class="meta">
                         <span class="price">$1200</span>
                         <span class="stay">1 Month</span>
@@ -18,6 +18,17 @@ export default function ShowCarDetails(props){
                     </div>
                 </div>
             </div>
+            <form class="ui form" onSubmit={(e)=>props.handleRentCar(e)}>
+                <div class="field">
+                    <label>Start Date ex:08/10/19 should be 081019</label>
+                    <input type="number" name="start_date" placeholder="start date" ></input>
+                </div>
+                <div class="field">
+                    <label>End Date ex:08/12/19 should be 081219</label>
+                    <input type="number" name="end_date" placeholder="end date" ></input>
+                </div>
+                <button class="ui button" type="submit">Submit</button>
+            </form>
         </div>
     )
 }
